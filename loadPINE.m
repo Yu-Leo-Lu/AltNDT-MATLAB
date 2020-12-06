6f9e081a4ea068ed5c1119ab0d47e26954ca9ca7
leoluyu0233 <51039032+leoluyu0233@users.noreply.github.com>
2020-12-05 19:23:05 -0800

    Update loadPINE.m

--- loadPINE.m
+++ loadPINE.m
@@ -1,12 +1,12 @@
-function [P,train_idx, test_idx] = loadPINE()
+function [P,inds_train, inds_test] = loadPINE()
 
 File   = 'PINE_train_val_test_dataset_IrinaOptimal.mat';
 P = load(File);  
 
-test_idx = P.inds_test;
-train_idx = P.inds_train{1,1};
-train_idx = union(train_idx, P.inds_train{2,1});
-train_idx = union(train_idx, P.inds_train{3,1});
-train_idx = union(train_idx, P.inds_train{4,1});
-train_idx = union(train_idx, P.inds_train{5,1});
+inds_test = P.inds_test;
+inds_train = P.inds_train{1,1};
+inds_train = union(inds_train, P.inds_train{2,1});
+inds_train = union(inds_train, P.inds_train{3,1});
+inds_train = union(inds_train, P.inds_train{4,1});
+inds_train = union(inds_train, P.inds_train{5,1});
 end
