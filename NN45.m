fbb7ef99123a646473ab23810ee2a748d63d34d3
leoluyu0233 <51039032+leoluyu0233@users.noreply.github.com>
2020-12-06 01:40:15 -0800

    Update NN45.m

--- NN45.m
+++ NN45.m
@@ -1,8 +1,8 @@
 [PINE, train_idx, test_idx] = loadPINE();
 X = PINE.data_all.X'; t = PINE.data_all.t';
 
-runs = 1;
-for i = 1:runs
+runs = 10;
+for i = 5:runs
     nn45 = feedforwardnet(45);
     nn45 = configure(nn45,X,t);
     nn45.divideFcn = 'divideind';
@@ -10,7 +10,7 @@ for i = 1:runs
     nn45.divideParam.testInd = test_idx;
     
     %train
-    nn45.trainParam.epochs = 40;
+    nn45.trainParam.epochs = 20;
     [nn45, nn45tr] = train(nn45,X,t);
-    save(sprintf('nn45_alltrain40eps_%d.mat',i), 'nn45', 'nn45tr')
+    save(sprintf('nn45_20eps_%d.mat',i), 'nn45', 'nn45tr')
 end
